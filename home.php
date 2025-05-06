30645bc76099e88f69b6b52f3e63fbc3776165b7
Why <116372391+Theminercodeer@users.noreply.github.com>
2025-05-06 15:26:19 -0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,28 @@
+<?php 
+    include_once 'header.php';
+?>
+
+        <div class="homepg">
+            <div class="content">
+                <p id="News">Game News</p>
+
+                <iframe class="video_news" src="https://www.youtube.com/embed/MHwgretY6mA?si=3pz0iB3OgNqERxAb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <p>Get ready for Eternal Sugar</p>
+                <iframe class="video_news" src="https://www.youtube.com/embed/4XDq2RLoTQs?si=z9JF2KPodIFUClvE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <div class="codelist">
+                    <ul>
+                        <lable><h>Codes</h></lable>
+                        <li>CKXBCMINIGAME  <b>(new)</b></li>
+                        <li>CKBEASTSUCCESS05</li>
+                        <li>KINGDOM1MILLION</li>
+                        <li>FIVEHUNDREDINSTA</li>
+                        <li>SUNNYLIUCOUPON4U</li>
+                        <li>3HUNDREDKDISCORD</li>
+                        <button><a href="https://coupon.devplay.com/coupon/ck/en" target="_blank" style="text-decoration: none; color: white; background-color: green; border: none; ">Redeem</a></button>
+                    </ul>
+                </div>
+            </div>
+        </div>
+</body>
+
+</html>
